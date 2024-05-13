85f39555e6fc79adaf8b76a3356a4143143472c4
Christianmagni97 <christianmagni28@gmail.com>
2024-05-13 23:09:21 +0200

    aggiutno parentesi graffa

--- Saltare a piè dispari/script.js
+++ Saltare a piè dispari/script.js
@@ -1,7 +1,7 @@
+const number = [];
 for (let index = 0 ; index < 6 ; index++){
     const userNumber = Number.parseInt( prompt('Type a number'), 10);
     if (userNumber % 2 numbersArray. push(userNumber);) {
-        numbersArray. push(userNumber);
-    
-
+        number. push(userNumber);
     }
+}    
